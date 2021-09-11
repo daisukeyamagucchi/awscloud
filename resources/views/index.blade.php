86f1a3bb6ffe188c07209bb307f59d15f57164ca
daisukeyamagucchi <dyamaguci@icloud.com>
2021-09-11 15:23:26 +0900

    適切なコミットコメント

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <h1>Blog Nam</h1>
+　　　　[<a href='/posts/create'>create</a>]
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
